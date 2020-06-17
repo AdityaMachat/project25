ad89846f2d9a28b628aee52b87b8812b54c0cd0e
AdityaMachat <64791200+AdityaMachat@users.noreply.github.com>
2020-06-17 11:17:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,40 @@
-var ground
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-//function preload()
-//{}
-var paper;
+
 function setup() {
 	createCanvas(800, 700);
 	engine = Engine.create();
 	world = engine.world;
 	
-	paper= new Papper(200,600);
-	ground= new Dustbin(width/2,height-50,width,5);
-	box1= new Dustbin(600,height-50,200,20);
-	box2= new Dustbin(700,height-90,20,100);
-    box3= new Dustbin(500,height-90,20,100);
+	paper=new Papper(200,600,20);
+	ground=new Ground (width/2,height-50,width,5);
+	box1=new Ground (600,height-50,200,10);
+	box2= new Ground(700,height-100,10,100);
+	box3= new Ground(500,height-100,10,100);
+	box4=new dustbin(600,600,200,90);
+
+
 	Engine.run(engine); 
 }
   
 
 function draw() {
-  background(150);
-  
-  paper.display();
+  background(200);
   box1.display();
   box2.display();
   box3.display();
+  box4.display();
+  paper.display();
   ground.display(); 
 }
 function keyPressed(){
 	if(keyCode === UP_ARROW){
 		Matter.Body.applyForce(paper.body,paper.body.position,{x:45,y:-65});
-		console.log(paper.Body)
+		
 
 	}
 }
