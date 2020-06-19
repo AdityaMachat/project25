97fcbfb46b890345f2c194df67089555a9065da1
AdityaMachat <64791200+AdityaMachat@users.noreply.github.com>
2020-06-19 07:47:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,19 +4,20 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
+const Constraint = Matter.Constraint;
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(2000, 700);
 	engine = Engine.create();
 	world = engine.world;
 	
-	paper=new Papper(200,600,20);
+	paper=new Papper(200,600,40);
 	ground=new Ground (width/2,height-50,width,5);
-	box1=new Ground (600,height-50,200,10);
-	box2= new Ground(700,height-100,10,100);
-	box3= new Ground(500,height-100,10,100);
-	box4=new dustbin(600,600,200,90);
-
+	box1=new Ground (1000,height-50,200,10);
+	box2= new Ground(1100,height-150,10,200);
+	box3= new Ground(900,height-150,10,200);
+	box4=new dustbin(1000,550,200,190);
+	slingshot = new Launcher(paper.body,{x:200,y:400});
 
 	Engine.run(engine); 
 }
@@ -33,11 +34,20 @@ function draw() {
 }
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:45,y:-65});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:25,y:-55});
 		
 
 	}
 }
+function mouseDragged(){
+    Matter.Body.setPosition(paper.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+slingshot.fly();
+    
+}
+
 
 
 
